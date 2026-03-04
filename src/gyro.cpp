04965418f815408f2171eb0c8119b484bde5689a
MattNRG <mattma@taltech.ee>
2026-03-04 17:01:38 +0200

    Fixed and added setupGyro() function. Must be called before gyro can be used.

--- src/gyro.cpp
+++ src/gyro.cpp
@@ -7,7 +7,7 @@ MPU6500 IMU;
 calData calib = { 0 };  //Calibration data
 GyroData gyroData;
 
-void setup()
+void setupGyro()
 {
     Wire.begin();
     Wire.setClock(400000); //400khz clock
@@ -18,18 +18,18 @@ void setup()
 
     int err = IMU.init(calib, IMU_ADDRESS);
     if (err != 0) {
-        Serial.print("Error initializing IMU: ");
-        Serial.println(err);
-        while (true) {
-            ;
-        }
+
+        Serial.println("GYRO NOT FOUND!.");
+
+    } else {
+
+        Serial.println("Keep robot level.");
+        delay(5000);
+        IMU.calibrateAccelGyro(&calib);
+        Serial.println("Calibration done!");
+        IMU.setGyroRange(500);      //USE THESE TO SET THE RANGE, IF AN INVALID RANGE IS SET IT WILL RETURN -1
+        IMU.setAccelRange(2);
     }
-    Serial.println("Keep robot level.");
-    delay(5000);
-    IMU.calibrateAccelGyro(&calib);
-    Serial.println("Calibration done!");
-    IMU.setGyroRange(500);      //USE THESE TO SET THE RANGE, IF AN INVALID RANGE IS SET IT WILL RETURN -1
-    IMU.setAccelRange(2);
 }
 
 int currentRotation = 0;
