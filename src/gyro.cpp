133a1fcd17a3d79ee3b4a8089ba4294debff738f
MattNRG <mattma@taltech.ee>
2026-03-04 15:56:50 +0200

    Added gyro support, added functions to get GyroRotation (possible to zero before use) and GyroTemp.

---
+++ src/gyro.cpp
@@ -0,0 +1,57 @@
+#include "FastIMU.h"
+#include <Wire.h>
+
+#define IMU_ADDRESS 0x68
+MPU6500 IMU;
+
+calData calib = { 0 };  //Calibration data
+GyroData gyroData;
+
+void setup()
+{
+    Wire.begin();
+    Wire.setClock(400000); //400khz clock
+    Serial.begin(115200);
+    while (!Serial) {
+        ;
+    }
+
+    int err = IMU.init(calib, IMU_ADDRESS);
+    if (err != 0) {
+        Serial.print("Error initializing IMU: ");
+        Serial.println(err);
+        while (true) {
+            ;
+        }
+    }
+    Serial.println("Keep robot level.");
+    delay(5000);
+    IMU.calibrateAccelGyro(&calib);
+    Serial.println("Calibration done!");
+    IMU.setGyroRange(500);      //USE THESE TO SET THE RANGE, IF AN INVALID RANGE IS SET IT WILL RETURN -1
+    IMU.setAccelRange(2);
+}
+
+int currentRotation = 0;
+
+int getGyroRotation(bool Zero){
+    IMU.update();
+    IMU.getGyro(&gyroData);
+
+    if (Zero){
+        currentRotation = 0;
+    }
+
+    currentRotation += round(gyroData.gyroZ / 11.11);
+
+    return currentRotation;
+}
+
+int getGyroTemp(){
+    if (IMU.hasTemperature())
+    {
+        return IMU.getTemp();
+    }
+
+    return 0;
+}
