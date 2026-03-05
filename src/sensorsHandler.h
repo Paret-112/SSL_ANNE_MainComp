346b77578f128bf60ea3f319ca9ccdaf6a7801ab
MattNRG <mattma@taltech.ee>
2026-03-05 09:21:22 +0200

    Added sensorsHandler.h, may not work.

---
+++ src/sensorsHandler.h
@@ -0,0 +1,12 @@
+//
+// Created by Mattias on 05/03/2026.
+//
+
+#ifndef SSL_ANNE_MAIN_SENSORSHANDLER_H
+#define SSL_ANNE_MAIN_SENSORSHANDLER_H
+
+int getGyroRotation(bool);
+int getGyroTemp();
+int getVoltage();
+
+#endif //SSL_ANNE_MAIN_SENSORSHANDLER_H
